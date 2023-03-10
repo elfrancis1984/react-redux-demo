f4dd6d85a524fc379ae8eb38f823f43479949b76
Francisco <devsox@Franciscos-MacBook-Pro.local>
2023-03-10 12:01:25 -0500

    mapStateToProps

--- src/App.js
+++ src/App.js
@@ -6,11 +6,14 @@ import HooksCakeContainer from './components/HooksCakeContainer';
 import IceCreamContainer from './components/IceCreamContainer';
 import HooksIcecreamContainer from './components/HooksIcecreamContainer';
 import NewCakeContainer from './components/NewCakeContainer';
+import ItemContainer from './components/ItemContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <ItemContainer cake />
+        <ItemContainer/>
         <HooksCakeContainer />
         <CakeContainer />
         <HooksIcecreamContainer />

873f8d8a72cf9ce580b558487a6f89ff6b5b7a22
Francisco <devsox@Franciscos-MacBook-Pro.local>
2023-03-10 13:33:21 -0500

    add mapDispatchToProps

--- src/components/ItemContainer.js
+++ src/components/ItemContainer.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { buyCake, buyIcecream } from '../redux'
 function ItemContainer(props) {
   return (
     <div>
       <h2>Item - {props.item}</h2>
+      <button onClick={props.dispatch}>Buy item</button>
     </div>
   )
 }
@@ -15,4 +17,15 @@ const mapStateToProps = (state, ownProps) => {
   }
 }
 
-export default connect(mapStateToProps)(ItemContainer)
+const mapDispatchToProps = (dispatch, ownProps) => {
+  const dispatchFunction = ownProps.cake
+    ? () => dispatch(buyCake())
+    : () => dispatch(buyIcecream())
+    return {
+      dispatch: dispatchFunction
+    }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps)(ItemContainer)
